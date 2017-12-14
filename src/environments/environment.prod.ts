866b38b0395326d1f60bb0d57b070d1fa7797cb8
ghorai77 <ghorai77@gmail.com>
2017-12-14 19:47:40 +0530

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+   apiKey: "AIzaSyA205vncjVKyMNGWFLNbJtNoCStK6O0arI",
+      authDomain: "first-45ad4.firebaseapp.com",
+      databaseURL: "https://first-45ad4.firebaseio.com",
+      projectId: "first-45ad4",
+      storageBucket: "first-45ad4.appspot.com",
+      messagingSenderId: "811759470204"
+  }
+};
